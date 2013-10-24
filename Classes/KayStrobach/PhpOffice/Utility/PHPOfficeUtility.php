6b299551c490bf1511d5fdea17e66befe116d105
Kay Strobach <github@kay-strobach.de>
2013-10-24 17:19:56 +0200

    [TASK] Import Utility

---
+++ Classes/KayStrobach/PhpOffice/Utility/PHPOfficeUtility.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace KayStrobach\PhpOffice\Utility;
+
+class PHPOfficeUtility implements t3lib_Singleton{
+	/**
+	 * contains the already initilized libaries
+	 */
+	protected $initialized = array();
+
+	/**
+	 *
+	 *
+	 * @param null $library
+	 * @return null
+	 * @throws Exception
+	 */
+	public function init($library = NULL) {
+		$path         = __DIR__ . '/../../../../Resources/Private/PHP/' . $library . '/Classes/' . $library . '.php';
+		$fallbackPath = __DIR__ . '/../../../../Resources/Private/PHP/' . $library . '/src/' . $library . '.php';
+		if(!in_array($library, $this->initialized)) {
+			if(file_exists($path)) {
+				include_once($path);
+				if(class_exists($library)) {
+					return new $library();
+				}
+			} elseif(file_exists($fallbackPath)) {
+				include_once($fallbackPath);
+				if(class_exists($library)) {
+					return new $library();
+				}
+			} else {
+				throw new Exception('Can´t find library "' . $library . '" in "' . $path . '"');
+			}
+		}
+		return NULL;
+	}
+}
