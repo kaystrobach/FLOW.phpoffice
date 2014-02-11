589a4c7b2a5f57640e0d4b4808f458d429cdfb2c
Kay Strobach <github@kay-strobach.de>
2014-02-11 07:11:57 +0100

    Fix: Cannot redeclare class PHPExcel

--- Classes/KayStrobach/PhpOffice/Utility/PHPOfficeUtility.php
+++ Classes/KayStrobach/PhpOffice/Utility/PHPOfficeUtility.php
@@ -19,20 +19,19 @@ class PHPOfficeUtility {
 		$path         = FLOW_PATH_PACKAGES . 'Libraries/phpoffice/' . strtolower($library) . '/Classes/' . $library . '.php';
 		$fallbackPath = FLOW_PATH_PACKAGES . 'Libraries/phpoffice/' . strtolower($library) . '/src/' . $library . '.php';
 		$classname    = '\\' . $library;
-		if(!in_array($library, $this->initialized)) {
-			if(file_exists($path)) {
-				include_once($path);
-				if(class_exists($classname)) {
-					return new $classname();
+		if(!class_exists($classname)) {
+			if(!in_array($library, $this->initialized)) {
+				if(file_exists($path)) {
+					include_once($path);
+				} elseif(file_exists($fallbackPath)) {
+					include_once($fallbackPath);
+				} else {
+					throw new \Exception('Can´t find library "' . $library . '" in "' . $path . '"');
 				}
-			} elseif(file_exists($fallbackPath)) {
-				include_once($fallbackPath);
-				if(class_exists($classname)) {
-					return new $classname();
-				}
-			} else {
-				throw new \Exception('Can´t find library "' . $library . '" in "' . $path . '"');
 			}
+		}
+		if(class_exists($classname)) {
+			return new $classname();
 		}
 		return NULL;
 	}
