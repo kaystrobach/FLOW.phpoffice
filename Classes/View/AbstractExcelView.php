3634a7732768a99e6e6e542e954ba60a046b67dd
Kay Strobach <github@kay-strobach.de>
2019-09-06 09:40:00 +0200

    Update AbstractExcelView.php

--- Classes/View/AbstractExcelView.php
+++ Classes/View/AbstractExcelView.php
@@ -108,6 +108,7 @@ class AbstractExcelView extends \Neos\Flow\Mvc\View\AbstractView
         header('Cache-Control: max-age=0');
 
         $objWriter = \PHPExcel_IOFactory::createWriter($excelFileObject, $this->getOption('writer'));
+        $this->configureWriter($objWriter);
         ob_start();
         $objWriter->save('php://output');
         return ob_get_clean();
@@ -175,5 +176,10 @@ class AbstractExcelView extends \Neos\Flow\Mvc\View\AbstractView
     {
         $date = new \DateTime('now');
         return $date->format('d.m.Y-H_i_s');
+    }
+    
+    protected function configureWriter(\PHPExcel_Writer_IWriter $writer)
+    {
+        return $writer;
     }
 }
